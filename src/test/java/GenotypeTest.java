38563d5e1a075d88d3bcee7a51eec6db6c22552b
Patka <patrycjamarkiewicz1203@gmail.com>
2025-12-23 23:03:53 +0100

    przekazwyanie parametrow + poprawnione testy

--- src/test/java/GenotypeTest.java
+++ src/test/java/GenotypeTest.java
@@ -1,3 +1,5 @@
+import agh.SimulationConfig;
+import agh.model.JungleWorldMap;
 import agh.model.Vector2d;
 import agh.model.animal.Animal;
 import agh.model.animal.Genotype;
@@ -9,16 +11,21 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class GenotypeTest {
+    SimulationConfig config = new SimulationConfig(new JungleWorldMap(10, 10), 2,
+            2, 5, 5, 100, 5,
+            55, 22, 1, 4, 10);
+
+
     @Test
     void genotypeShouldHaveCorrectLength() {
-        Genotype genotype = new Genotype();
+        Genotype genotype = new Genotype(config);
 
-        assertEquals(10, genotype.getGenom().size());
+        assertEquals(config.genotypeLen(), genotype.getGenom().size());
     }
 
     @Test
     void genesShouldBeInCorrectRange() {
-        Genotype genotype = new Genotype();
+        Genotype genotype = new Genotype(config);
 
         for (int gene : genotype.getGenom()) {
             assertTrue(gene >= 0 && gene <= 7);
@@ -26,10 +33,19 @@ public class GenotypeTest {
     }
 
     @Test
-    void activeGenomIndexShouldWrapAround() {
-        Genotype genotype = new Genotype();
+    void correctActiveGenomIndex() {
+        Genotype genotype = new Genotype(config);
 
-        for (int i = 0; i < 10; i++) {
+        genotype.nextGenomIndex();
+
+        assertEquals(1, genotype.getActievGenomIndex());
+    }
+
+    @Test
+    void activeGenomIndexShouldWrapAround() {
+        Genotype genotype = new Genotype(config);
+
+        for (int i = 0; i < config.genotypeLen(); i++) {
             genotype.nextGenomIndex();
         }
 
@@ -38,12 +54,12 @@ public class GenotypeTest {
 
     @Test
     void crossGenotypeShouldCreateValidChildGenotype() {
-        Animal parent1 = new Animal(new Vector2d(0, 0));
-        Animal parent2 = new Animal(new Vector2d(1, 1));
+        Animal parent1 = new Animal(new Vector2d(0, 0), config);
+        Animal parent2 = new Animal(new Vector2d(1, 1), config);
 
-        Genotype child = Genotype.crossGenotype(parent1, parent2);
+        Genotype child = Genotype.crossGenotype(parent1, parent2, config);
 
-        assertEquals(10, child.getGenom().size());
+        assertEquals(config.genotypeLen(), child.getGenom().size());
 
         for (int gene : child.getGenom()) {
             assertTrue(gene >= 0 && gene <= 7);
@@ -53,7 +69,7 @@ public class GenotypeTest {
     @Test
     void mutationChangesSomeGenesCorrectly() {
         List<Integer> genes = List.of(0,0,0,0,0,0,0,0,0,0);
-        Genotype genotype = new Genotype(genes);
+        Genotype genotype = new Genotype(genes, config);
 
         boolean changed = false;
 
